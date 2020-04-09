a141595a0f731b94bc59bb3c0e18be5b7c7fcd5b
Jacob Carlone <carlone.jacob@gmail.com>
2020-04-09 19:50:23 +0000

    refactored function to not have any console logs and only return statements

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
 const request = require('request');
-let args = process.argv.slice(2);
-request(`https://api.thecatapi.com/v1/breeds/search?=q${args}`, (error, response, body) => {
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (error !== null) {
-    return console.log(error);
-  }
-  if (data.length === 0) {
-    return console.log("breed not found!");
-  }
-  console.log(data[0]['description']);
+    if (error) {
+      return callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback("breed not found", null);
+    }
+    return callback(null, data[0]['description']);
   
-});
+  });
+};
+
+module.exports = {fetchBreedDescription};
+
+
